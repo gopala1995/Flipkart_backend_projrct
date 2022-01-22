86f0fd52779bed22a8d630430605b26b376041b9
Gopala <guptaraja727@gmail.com>
2022-01-22 12:26:16 +0530

    cc

--- src/controlars/product.controlar.js
+++ src/controlars/product.controlar.js
@@ -4,8 +4,8 @@ const router = express.Router()
 
 const Product = require("../models/product.model")
 
-router.get("",async(req,res)=>{
-    const product = await Product.find().lean().exec()
+router.get("/:id",async(req,res)=>{
+    const product = await Product.findById(req.params.id).lean().exec()
     return res.status(200).send(product)
 })
 
